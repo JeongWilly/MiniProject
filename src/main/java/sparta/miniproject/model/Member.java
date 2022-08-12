74c444940ea74b521848fb07efb6ca5a0a4865a0
Minkyu222341 <qkralsrb2040@naver.com>
2022-08-12 22:08:22 +0900

    베이스파일

---
+++ src/main/java/sparta/miniproject/model/Member.java
@@ -0,0 +1,37 @@
+package sparta.miniproject.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sparta.miniproject.Timestamped;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "member")
+@Entity
+public class Member extends Timestamped {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY) //GenerationType.IDENTITY : ID값이 서로 영향없이 자기만의 테이블 기준으로 올라간다.
+   private Long id;
+   @Column(nullable = false, unique = true)
+   private String nickname;
+   @JsonIgnore
+   @Column(nullable = false)
+   private String password;
+
+   @Enumerated(EnumType.STRING)
+   private Authority authority;
+
+
+
+   @Builder
+   public Member(String nickname, String password, Authority authority) {
+      this.nickname = nickname;
+      this.password = password;
+      this.authority = authority;
+   }
+}
